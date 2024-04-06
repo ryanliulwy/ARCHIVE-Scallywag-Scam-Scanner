096673441fd9cd683e8f6cec3074dd4b418a44ac
Andrew Smithwick <andy.smithwick@gmail.com>
2024-04-06 15:43:57 -0700

    Image input working

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,33 @@
-import storeInfo from "./storeInfo";
+'use client'
+
 import styles from "./page.module.css";
+import { useState, useRef } from "react";
 
 export default function Home() {
+
+  const [file, setFile] = useState('');
+  const inputRef = useRef();
+
+
+
   return (
     <main className={styles.main}>
       <h1 className={styles.store_header}>Pirate Marketplace</h1>
+      <label htmlFor="myfile">Select a file:</label>
+      <input type="file" id="myfile" name="myfile" accept=".png, .jpg, .jpeg, .webp, .heic, .heif" onInput={() => setFile(URL.createObjectURL(inputRef.current.files[0]))}
+                ref={inputRef}>
+
+                </input>
+      {file ? 
+        <div><p>I did things</p>
+        <img src={file} />
+      </div>
+       : null}
     </main>
   );
 }
+
+
+
+// Image types: https://ai.google.dev/docs/gemini_api_overview#image_requirements
+// Get Input Value: https://bobbyhadz.com/blog/react-get-input-value
